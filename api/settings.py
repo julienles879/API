eb3bd219c0fa6e46221e46cd2254574decb8879e
jlesimple <julienlesimple1996@gmail.com>
2023-11-06 09:38:01 +0100

    UPD settings

--- api/settings.py
+++ api/settings.py
@@ -19,6 +19,7 @@ env = environ.Env()
 environ.Env.read_env()
 config = Config('/.env')
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,9 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!config = Config('/.env')
 
-SECRET_KEY = config
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
